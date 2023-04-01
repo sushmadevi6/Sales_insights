d689428a3e96ab483860d19220fa057bc49ed642
sushmadevi6 <108875040+sushmadevi6@users.noreply.github.com>
2023-04-01 11:19:59 +0530

    Delete Sales_insights directory

--- Sales_insights/sales.sql
+++
@@ -1,4 +0,0 @@
-select distinct(transactions.currency) from transactions;
-select count(*) from transactions where currency='INR\r';
-select count(*) from transactions where currency='INR';
-select count(*) from transactions where currency='USD\r' or currency='USD';
